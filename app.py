53899562a34dcd13c455b978d4793a56863d0169
Shubham Mallick <shubhammallick678@gmail.com>
2024-12-09 23:38:23 +0530

    first commit

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, request, app, render_template
-from flask import Response
+from flask import Flask, request, render_template
 import pickle
 import numpy as np
-import pandas as pd
-import os
 
 application = Flask(__name__)
 app = application
 
+# Load scaler and model
 scaler = pickle.load(open(r"C:\Users\aditya\Desktop\Pregrad\Pregrad_august\Model\standardScaler.pkl", "rb"))
 model = pickle.load(open(r"C:\Users\aditya\Desktop\Pregrad\Pregrad_august\Model\modelForPrediction.pkl", "rb"))
 
@@ -15,33 +13,34 @@ model = pickle.load(open(r"C:\Users\aditya\Desktop\Pregrad\Pregrad_august\Model\
 def index():
     return render_template('index.html')
 
-@app.route('predictdata', methods = ['GET', 'POST'])
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    result= ""
+    result = ""
 
     if request.method == 'POST':
-        
-        Pregancies = int(request.form.get('Pregnancies'))
-        Glucose = float(request.form.get('Glucose'))
-        BloodPressure = float(request.form.get('BloodPressure'))
-        SkinThickness = float(request.form.get('SkinThickness'))
-        Insulin = float(request.form.get('Insulin'))
-        BMI = float(request.form.get('BMI'))
-        DiabetesPedigreeFunction = float(request.form.get('DiabetesPedigreeFunction'))
-        Age = float(request.form.get('Age'))
+        # Gather inputs from the form
+        Temperature = float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        BUI = float(request.form.get('BUI'))
+        FWI = float(request.form.get('FWI'))
 
-        new_data = scaler.transform([[Pregancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        # Preprocess and predict
+        new_data = scaler.transform([[Temperature, RH, Ws, Rain, FFMC, DMC, ISI, BUI, FWI]])
         predict = model.predict(new_data)
 
-        if predict[0] --1:
-            result = 'Diabetic'
+        if predict[0] == 1:
+            result = 'Fire'
         else:
-            result = 'Non-Diabetic'
+            result = 'No Fire'
 
-        return render_template('single_prediction.html', result = result)
-    
+        return render_template('home.html', result=result)
     else:
-        return render_template('home.html')
-    
-if __name__=="__main__":
-    app.run(host = "0.0.0.0", debug = True)
+        return render_template('home.html', result=result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
