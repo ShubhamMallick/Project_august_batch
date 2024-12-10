ecd304a7c8a8729a0c4809c1f00128ba22557bb1
Shubham Mallick <shubhammallick678@gmail.com>
2024-12-10 18:32:06 +0530

    model training

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 import pickle
+import os
 import numpy as np
 
 application = Flask(__name__)
@@ -8,6 +9,15 @@ app = application
 # Load scaler and model
 scaler = pickle.load(open(r"C:\Users\aditya\Desktop\Pregrad\Pregrad_august\Model\standardScaler.pkl", "rb"))
 model = pickle.load(open(r"C:\Users\aditya\Desktop\Pregrad\Pregrad_august\Model\modelForPrediction.pkl", "rb"))
+
+# Favicon route
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
 
 @app.route('/')
 def index():
